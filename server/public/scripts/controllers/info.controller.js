4378977d265e88cc7924c2740ab730995c81fb44
Laura Goetz <goetzlaura612@gmail.com>
2017-11-17 19:53:04 -0600

    dialog box appearing on click

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,5 +1,18 @@
-myApp.controller('InfoController', function(UserService) {
+myApp.controller('InfoController', function(UserService,$mdDialog) {
   console.log('InfoController created');
   var vm = this;
   vm.userService = UserService;
+
+  vm.resourceAlert = function(ev) {
+    console.log('dialog box');
+    $mdDialog.show({
+      controller: 'InfoController',
+      templateUrl: 'views/templates/resourceDialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+    })
+  }
+
+
 });
