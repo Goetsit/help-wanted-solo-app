0496ccc055ec0fcebf2d01d08cb3708e4c5d58f9
Laura Goetz <goetzlaura612@gmail.com>
2017-11-19 21:31:32 -0600

    left joined

--- server/routes/info.router.js
+++ server/routes/info.router.js
@@ -18,7 +18,7 @@ router.get('/', function (req, res) {
       console.log('Error connecting to db', errorConnectingToDB);
       res.sendStatus(500);
     } else {
-      var queryText = 'SELECT r."resourceid",r."resourcename", r."description",r."imageurl" ,r."phone",r."website" ,h."mondayhours" ,h."tuesdayhours",h."wednesdayhours",h."thursdayhours",h."fridayhours" ,h."saturdayhours",h."fridayhours",h."saturdayhours",h."sundayhours", a.* FROM public."resources" r LEFT JOIN public."hoursofoperation" h ON h.resourceid = r.resourceid LEFT JOIN public."resourceaddress" ra ON ra."resourceid" = r."resourceid"  INNER JOIN public."addresses" a ON a."addressid" = ra."addressid"WHERE "isapproved" = TRUE';
+      var queryText = 'SELECT r."resourceid",r."resourcename", r."description",r."imageurl" ,r."phone",r."website" ,h."mondayhours" ,h."tuesdayhours",h."wednesdayhours",h."thursdayhours",h."fridayhours" ,h."saturdayhours",h."fridayhours",h."saturdayhours",h."sundayhours", a.* FROM public."resources" r LEFT JOIN public."hoursofoperation" h ON h.resourceid = r.resourceid LEFT JOIN public."resourceaddress" ra ON ra."resourceid" = r."resourceid"  LEFT JOIN public."addresses" a ON a."addressid" = ra."addressid"WHERE "isapproved" = TRUE';
       db.query(queryText, function (errorMakingQuery, result) {
         done();
         if (errorMakingQuery) {
